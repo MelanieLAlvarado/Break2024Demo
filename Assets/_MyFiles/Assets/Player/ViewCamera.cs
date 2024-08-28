4c85070b0e03916915b4d7975637fbd592458b47
Alvarado <mealvara@student.uiwtx.edu>
2024-08-28 17:50:15 -0500

    saved scene

--- Assets/_MyFiles/Assets/Player/ViewCamera.cs
+++ Assets/_MyFiles/Assets/Player/ViewCamera.cs
@@ -6,7 +6,7 @@ public class ViewCamera : MonoBehaviour
 {
     [SerializeField] private Transform pitchTransform;
     [SerializeField] private Camera viewCamera;
-    [SerializeField] private float armLength = 7f;g
+    [SerializeField] private float armLength = 7f;
 
     private Transform _parentTransform;
     
